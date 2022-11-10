1908ee7192e6764b66aab2433447c97a3cdb919e
leroy-netizen <leroythomas051@gmail.com>
2022-11-10 15:15:38 +0300

    build and create route to 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, Redirect} from 'react-router-dom'
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
+import Four_oh_Four from "./components/Four_oh_Four";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/not-found" element={<Four_oh_Four />} />
+        {/* <Redirect to="/not-found" /> */}
       </Routes>
     </div>
   );
---
+++ src/components/Four_oh_Four.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Four_oh_Four = () => {
+  return (
+      <div className='not-found'>
+          <h1>404</h1>
+          <p>Page Not Found</p>
+    </div>
+  )
+}
+
+export default Four_oh_Four
